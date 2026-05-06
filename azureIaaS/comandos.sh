937cfa7d58674f0de7c274202ae5094958b7f993
David Martinez <davidmartinezosorio@gmail.com>
2026-05-06 15:20:54 -0400

    Add the azureIaaS folder with commands

--- azureIaaS/comandos.sh
+++ azureIaaS/comandos.sh
@@ -10,7 +10,7 @@ az vm create -n iaas-vm-amin -g GrupoRecursosIaaS --image Ubuntu2204 --admin-use
 az storage account create -n storageiaas004 -g GrupoRecursosIaaS -l eastus2 --sku Standard_LRS
 
 # Crear una red virtual
-az network vnet create -g GrupoRecursosIaaS -n IaaSVnet004 --address-prefix 10.0.0.0/16 --subnet-name aminesSubnet --subnet-prefixes 10.0.0.0/24
+az network vnet create -g GrupoRecursosIaaS -n IaaSVnet004 --address-prefix 10.0.0.0/16 --subnet-name dmartinezSubnet --subnet-prefixes 10.0.0.0/24
 
 #Eliminar todos los recursos creados
 az group delete -n GrupoRecursosIaaS --force-deletion-types Microsoft.Compute/virtualMachines
